2ce3855742d748f0e17b51f1351254c113367e86
Liru <theliru@gmail.com>
2017-07-13 19:58:36 -0400

    Initialize git repository

---
+++ lib/wolfypet.ex
@@ -0,0 +1,18 @@
+defmodule Wolfypet do
+  @moduledoc """
+  Documentation for Wolfypet.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wolfypet.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/wolfypet_test.exs
@@ -0,0 +1,8 @@
+defmodule WolfypetTest do
+  use ExUnit.Case
+  doctest Wolfypet
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
