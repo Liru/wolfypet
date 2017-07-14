d91de68a594284794280e38e7108ddb1b544be89
Liru <theliru@gmail.com>
2017-07-13 22:09:19 -0400

    Add Ecto and supervision tree for application

---
+++ lib/wolfypet/repo.ex
@@ -0,0 +1,3 @@
+defmodule Wolfypet.Repo do
+  use Ecto.Repo, otp_app: :wolfypet
+end
--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule Wolfypet.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger],
+     mod: {Wolfypet.Application, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +29,7 @@ defmodule Wolfypet.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:postgrex, ">= 0.0.0"},
+     {:ecto, "~> 2.1"}]
   end
 end
